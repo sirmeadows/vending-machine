5a9d9dd40ccb461ede57e74f1267e69aeaa27689
Mark Meadows <markk360@live.com>
2020-06-14 16:29:05 +0100

    Vending System completion with full testing

--- VendingMachine/VendingMachine/Logic/VendingSystem.cs
+++ VendingMachine/VendingMachine/Logic/VendingSystem.cs
@@ -1,53 +1,146 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using VendingMachine.Models;
+using VendingMachine.Models.Enum;
 
 namespace VendingMachine.Logic
 {
     public class VendingSystem
     {
-        public VendingSystem(List<Product> products, bool noChange = false)
+        public VendingSystem(List<Product> products, List<Coin> bankedCoins)
         {
             Products = products;
-            NoChange = noChange;
-            Display = "INSERT COIN";
+            BankedCoins = bankedCoins;
+            HasChangeAvailable();
+            Display = NoChange ? "EXACT CHANGE ONLY" : "INSERT COIN";
         }
 
         public string Display { get; set; }
         public List<Product> Products { get; set; }
-        public List<Coin> Coins { get; set; } = new List<Coin>();
+        public List<Coin> InsertedCoins { get; set; } = new List<Coin>();
+        public List<Coin> BankedCoins { get; set; }
+        public Dictionary<CoinType, int> CoinsToDispense { get; set; }
         public decimal TotalValue { get; set; } = 0.00m;
-        public bool NoChange { get; set; }
+        public bool NoChange { get; set; } = false;
 
-        public void InsertCoin(decimal weight, decimal size)
+        public void CheckDisplay()
         {
-            var coin = new Coin(weight, size);
-
-            if (coin.CoinType == Models.Enum.CoinType.Invalid)
+            if (TotalValue <= 0.00m)
             {
-                return;
+                Display = NoChange ? "EXACT CHANGE ONLY" : "INSERT COIN";
             }
+            else
+            {
+                Display = $"${TotalValue.ToString("0.00")}";
+            }
+        }
 
-            Coins.Add(coin);
+        public void InsertCoin(Coin coin)
+        {
+            InsertedCoins.Add(coin);
             TotalValue += coin.Value;
-            Display = TotalValue.ToString();
+            Display = $"${TotalValue.ToString("0.00")}";
         }
 
         public void ReturnCoins()
         {
-            foreach (var coin in Coins)
+            foreach (var coin in InsertedCoins)
             {
-                // return to customer
+                // TODO: Return to customer
             }
 
-            Coins = new List<Coin>();
-            Display = "INSERT COIN";
+            InsertedCoins = new List<Coin>();
+            Display = NoChange ? "EXACT CHANGE ONLY" : "INSERT COIN";
         }
 
         public void PurchaseProduct(int productId)
         {
+            var selectedProduct = Products.Where(product => product.Id == productId).FirstOrDefault();
 
+            if (selectedProduct == null)
+                return;
+
+            if (selectedProduct.Stock <= 0)
+            {
+                Display = "SOLD OUT";
+                return;
+            }
+
+            if (selectedProduct.Price > TotalValue)
+            {
+                Display = $"PRICE ${selectedProduct.Price.ToString("0.00")}";
+                return;
+            }
+
+            var change = MakeChange(selectedProduct.Price);
+
+            UpdateBankedCoins();
+
+            if (change > 0.00m) 
+            {
+                CalculateChangeInCoins(change);
+
+                // Dispense coins here
+
+                CoinsToDispense = new Dictionary<CoinType, int>();
+            }
+
+            Display = "THANK YOU";
+        }
+
+        public decimal MakeChange(decimal purchasePrice)
+        {
+            if (NoChange)
+                return 0.00m;
+
+            return TotalValue - purchasePrice;
+        }
+
+        void UpdateBankedCoins()
+        {
+            foreach (var coin in InsertedCoins)
+            {
+                BankedCoins.Add(coin);
+            }
+
+            InsertedCoins = new List<Coin>();
+        }
+
+        public void CalculateChangeInCoins(decimal change)
+        {
+            var coinsToReturn = new Dictionary<CoinType, int>();
+            var availableCoins = BankedCoins.GroupBy(coin => coin.CoinType);
+
+            var availableDimes = availableCoins.Where(coins => coins.Key == CoinType.Dime).FirstOrDefault() != null ? availableCoins.Where(coins => coins.Key == CoinType.Dime).FirstOrDefault().Count() : 0;
+            var availableNickels = availableCoins.Where(coins => coins.Key == CoinType.Nickel).FirstOrDefault() != null ? availableCoins.Where(coins => coins.Key == CoinType.Nickel).FirstOrDefault().Count() : 0;
+
+            var dimes = Math.Min(Math.Floor(change / 0.10m), availableDimes);
+            change -= 0.10m * dimes;
+
+            coinsToReturn.Add(CoinType.Dime, (int)dimes);
+
+            var nickels = Math.Min(Math.Floor(change / 0.05m), availableNickels);
+
+            coinsToReturn.Add(CoinType.Nickel, (int)nickels);
+
+            CoinsToDispense = coinsToReturn;
+        }
+
+        /// <summary>
+        /// To buy a product, there needs to be 1 Nickel (0.05) and 1 Dime (0.10) OR 3 Nickels
+        /// Most that can be expected is $1.00 for Cola, made of 4 Quarters. Customers buying other Products should enter 3 Quarters maximum.
+        /// </summary>
+        void HasChangeAvailable()
+        {
+            var nickels = BankedCoins.Where(coin => coin.CoinType == CoinType.Nickel).Count();
+            var dimes = BankedCoins.Where(coin => coin.CoinType == CoinType.Dime).Count();
+
+            if (!((dimes > 0 && nickels > 0) || nickels > 2))
+            {
+                NoChange = true;
+            }
         }
     }
 }
--- VendingMachine/VendingMachineTests/VendingSystemFacts.cs
+++ VendingMachine/VendingMachineTests/VendingSystemFacts.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using VendingMachine.Logic;
 using VendingMachine.Models;
+using VendingMachine.Models.Enum;
 using Xunit;
 
 namespace VendingMachineTests
@@ -10,6 +12,7 @@ namespace VendingMachineTests
     public class VendingSystemFacts
     {
         private List<Product> products;
+        private List<Coin> bankedCoins;
 
         public VendingSystemFacts()
         {
@@ -26,53 +29,212 @@ namespace VendingMachineTests
                 {
                     Id = 2,
                     Name = "Candy",
-                    Price = 1.00m,
+                    Price = 0.65m,
                     Stock = 1
                 },
                 new Product
                 {
                     Id = 3,
                     Name = "Chips",
-                    Price = 1.00m,
+                    Price = 0.60m,
                     Stock = 1
                 }
+            };
+
+            bankedCoins = new List<Coin>
+            {
+                new Coin(5.000m, 21.21m),
+                new Coin(5.000m, 21.21m),
+                new Coin(5.000m, 21.21m),
+                new Coin(2.268m, 17.91m)
             };
         }
 
         [Fact]
         public void Initiates()
         {
-            var system = new VendingSystem(products);
+            var system = new VendingSystem(products, bankedCoins);
             Assert.Equal(products, system.Products);
         }
 
         [Fact]
-        public void InitiatesWithNoChange()
+        public void InitiatesWithChange()
         {
-            var system = new VendingSystem(products, true);
+            var system = new VendingSystem(products, bankedCoins);
+            Assert.False(system.NoChange);
+        }
+
+        [Fact]
+        public void InitiatesWithNoChangeTwoNickelsNoDimes()
+        {
+            bankedCoins = new List<Coin>
+            {
+                new Coin(5.000m, 21.21m),
+                new Coin(5.000m, 21.21m),
+            };
+
+            var system = new VendingSystem(products, bankedCoins);
             Assert.Equal(products, system.Products);
+            Assert.Equal("EXACT CHANGE ONLY", system.Display);
+            Assert.True(system.NoChange);
+        }
+
+        [Fact]
+        public void InitiatesWithNoChangeNoNickelsOneDime()
+        {
+            bankedCoins = new List<Coin>
+            {
+                new Coin(2.268m, 17.91m)
+            };
+
+            var system = new VendingSystem(products, bankedCoins);
+            Assert.Equal(products, system.Products);
+            Assert.Equal("EXACT CHANGE ONLY", system.Display);
             Assert.True(system.NoChange);
         }
 
         [Fact]
         public void StartsWithInsertCoinDisplay()
         {
-            var system = new VendingSystem(products);
+            var system = new VendingSystem(products, bankedCoins);
             Assert.Equal("INSERT COIN", system.Display);
         }
 
         [Fact]
         public void StartsWithNoCoins()
         {
-            var system = new VendingSystem(products);
-            Assert.Empty(system.Coins);
+            var system = new VendingSystem(products, bankedCoins);
+            Assert.Empty(system.InsertedCoins);
         }
 
         [Fact]
         public void StartsWithNoTotalValue()
         {
-            var system = new VendingSystem(products);
+            var system = new VendingSystem(products, bankedCoins);
             Assert.Equal(0.00m, system.TotalValue);
+        }
+
+        [Theory]
+        [InlineData(0.05, 0, 1)]
+        [InlineData(0.1, 1, 0)]
+        [InlineData(0.15, 1, 1)]
+        public void DispensesChangeNickelsAndDimes(decimal change, int expectedNumberOfDimes, int expectedNumberOfNickels)
+        {
+            var system = new VendingSystem(products, bankedCoins);
+            system.CalculateChangeInCoins(change);
+            var nickels = system.CoinsToDispense.Where(coins => coins.Key == CoinType.Nickel).FirstOrDefault();
+            var dimes = system.CoinsToDispense.Where(coins => coins.Key == CoinType.Dime).FirstOrDefault();
+            Assert.Equal(expectedNumberOfNickels, nickels.Value);
+            Assert.Equal(expectedNumberOfDimes, dimes.Value);
+        }
+
+        [Theory]
+        [InlineData(0.05, 0, 1)]
+        [InlineData(0.1, 0, 2)]
+        [InlineData(0.15, 0, 3)]
+        public void DispensesChangeNickelsOnly(decimal change, int expectedNumberOfDimes, int expectedNumberOfNickels)
+        {
+            bankedCoins = new List<Coin>
+            {
+                new Coin(5.000m, 21.21m),
+                new Coin(5.000m, 21.21m),
+                new Coin(5.000m, 21.21m)
+            };
+
+            var system = new VendingSystem(products, bankedCoins);
+            system.CalculateChangeInCoins(change);
+            var nickels = system.CoinsToDispense.Where(coins => coins.Key == CoinType.Nickel).FirstOrDefault();
+            var dimes = system.CoinsToDispense.Where(coins => coins.Key == CoinType.Dime).FirstOrDefault();
+            Assert.Equal(expectedNumberOfNickels, nickels.Value);
+            Assert.Equal(expectedNumberOfDimes, dimes.Value);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void OutOfStock(int productId)
+        {
+            products.Where(product => product.Id == productId).First().Stock = 0;
+            var system = new VendingSystem(products, bankedCoins);
+
+            system.PurchaseProduct(productId);
+            Assert.Equal("SOLD OUT", system.Display);
+        }
+
+
+        [Theory]
+        [InlineData(1, 0.10, "PRICE $1.00")]
+        [InlineData(2, 0.10, "PRICE $0.65")]
+        [InlineData(3, 0.10, "PRICE $0.60")]
+        public void NotEnoughMoneyToPurchase(int productId, decimal initialMoney, string expectedDisplay)
+        {
+            var system = new VendingSystem(products, bankedCoins);
+            system.TotalValue = initialMoney;
+
+            system.PurchaseProduct(productId);
+            Assert.Equal(expectedDisplay, system.Display);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void PurchaseProduct(int productId)
+        {
+            var system = new VendingSystem(products, bankedCoins);
+            system.TotalValue = 1.00m;
+
+            system.PurchaseProduct(productId);
+            Assert.Equal("THANK YOU", system.Display);
+        }
+
+        [Theory]
+        [InlineData(1, 0.5)]
+        [InlineData(0.6, 0.9)]
+        [InlineData(0.65, 0.85)]
+        public void ReturnsChange(decimal purchasePrice, decimal expectedChange)
+        {
+            var system = new VendingSystem(products, bankedCoins);
+            system.TotalValue = 1.5m;
+
+            var change = system.MakeChange(purchasePrice);
+            Assert.Equal(expectedChange, change);
+        }
+
+        [Theory]
+        [InlineData(1, 0)]
+        [InlineData(0.6, 0)]
+        [InlineData(0.65, 0)]
+        public void ReturnsNoChange(decimal purchasePrice, decimal expectedChange)
+        {
+            bankedCoins = new List<Coin>
+            {
+                new Coin(5.000m, 21.21m),
+                new Coin(5.000m, 21.21m),
+            };
+
+            var system = new VendingSystem(products, bankedCoins);
+            system.TotalValue = 1.5m;
+
+            var change = system.MakeChange(purchasePrice);
+            Assert.Equal(expectedChange, change);
+        }
+
+        [Theory]
+        [InlineData(5.000, 21.21, CoinType.Nickel, 0.05)]
+        [InlineData(2.268, 17.91, CoinType.Dime, 0.10)]
+        [InlineData(5.670, 24.26, CoinType.Quarter, 0.25)]
+        public void InsertCoinUpdatesCoinListAndValueAndDisplay(decimal coinWeight, decimal coinSize, CoinType expectedCoinType, decimal expectedValue)
+        {
+            var coin = new Coin(coinWeight, coinSize);
+
+            var system = new VendingSystem(products, bankedCoins);
+            system.InsertCoin(coin);
+
+            Assert.True(system.InsertedCoins.Select(coin => coin.CoinType).First() == expectedCoinType);
+            Assert.True(system.InsertedCoins.Select(coin => coin.Value).First() == expectedValue);
+            Assert.Equal("$" + expectedValue.ToString("0.00"), system.Display);
         }
     }
 }
